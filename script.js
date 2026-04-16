5e894403e5b64f30bd5f5ace85e26a9d647d55b3
Sabaafrin Ayyub Sheikh <sabaafrins@cybage.com>
2026-04-16 13:23:53 +0530

    initial setup

---
+++ script.js
@@ -0,0 +1,10 @@
+function getAllLabels() {
+  return document.getElementsByTagName('label');
+}
+
+
+window.onload = function() {
+  // query for all html label tags
+  const labels = getAllLabels();
+};  
+ 
